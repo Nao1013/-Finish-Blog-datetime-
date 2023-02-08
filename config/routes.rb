22cb28e285c71423d372d6946e8b38b6d732e868
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-02-08 06:49:11 +0000

    今日のメニュー

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'show/index'
+  get 'menus/index'
+  get 'menus/show'
+  get 'menus/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get root :to => 'homes#top'
 end
--- test/controllers/show_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ShowControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get show_index_url
-    assert_response :success
-  end
-end
